caea429e0f897ce1e809058c88c3ade851506dff
sonbtph52402 <sonbtph52402@gmail.com>
2024-11-09 21:50:39 +0700

    du-an

---
+++ client/common/env.php
@@ -0,0 +1,15 @@
+<?php 
+
+// Biến môi trường, dùng chung toàn hệ thống
+// Khai báo dưới dạng HẰNG SỐ để không phải dùng $GLOBALS
+
+define('BASE_URL'       , 'http://localhost/Project_1/client/');    
+define('BASE_URL_ADMIN'       , 'http://localhost/D%e1%bb%b1%20%c3%a1n%201/Base-du-an-1/admin/');
+
+define('DB_HOST'    , 'localhost');
+define('DB_PORT'    , 3307);
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME'    , 'db');  // Tên database
+
+define('PATH_ROOT'    , __DIR__ . '/../');
