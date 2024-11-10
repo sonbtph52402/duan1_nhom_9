af337d8989854bf1d6b6a220cde0dfa3be91cd33
sonbtph52402 <sonbtph52402@gmail.com>
2024-11-10 22:51:52 +0700

    giaodien

--- client/index.php
+++ client/index.php
@@ -8,7 +8,7 @@ require_once './common/function.php'; // Hàm hỗ trợ
 require_once './controllers/HomeController.php';
 
 // Require toàn bộ file Models
-require_once './models/Student.php';
+// require_once './models/Student.php';
 
 // Route
 $act = $_GET['act'] ?? '/';
